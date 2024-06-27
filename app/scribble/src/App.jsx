468d2c68f34f3dda655e1bc84e866959f9d7d4c2
gitmarcher <marchingsamee@gmail.com>
2024-06-27 20:13:35 +0530

    feat:Integrted Vite, set up root CSS

---
+++ app/scribble/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-800">
+      <h1 className="text-slate-100 text-4xl">Hello World</h1>
+      <a>Clicke here</a>
+    </div>
+  );
+}
+
+export default App;
